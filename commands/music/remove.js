911e04be4156a6ddafabd0ccc8a320b15b508ef4
CORN Studio <CORNStudio@null.spigotmc.org>
2023-07-21 22:39:41 +0800

    remove command bug fix

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -12,10 +12,22 @@ module.exports = {
         .setRequired(true)
     }),
     async execute(interaction){
-        const guild_id = ctx.guild.id
+        const guild_id = interaction.guild.id
         const index = Number(interaction.options.getString("index"))
         if (index == NaN){
             await interaction.reply("index must be a number")
+            return
+        }
+        if (index <= 0){
+            await interaction.reply("index must > 0")
+            return
+        }
+        if (queue[guild_id] == undefined){
+            await interaction.reply("can't found queue please request a song")
+            return
+        }
+        if (index > queue[guild_id].length){
+            await interaction.reply("index of song is out of queue's range")
             return
         }
         const remove_song = queue[guild_id].splice(index - 1, 1)
